3871bc843a130849287e95cddd4d2eef99270d42
DmytroMaslo <maslo.dmitry@gmail.com>
2023-02-19 16:59:43 +0200

    components Header Technologies

--- src/App.js
+++ src/App.js
@@ -3,7 +3,23 @@ import './App.css';
 const App = () => {
   return (
     <div className="App">
-      Simple HTML
+      <Header />
+      <Technologies/>
+    </div>
+  );
+}
+const Header = () => {
+  return (
+    <div className="header">
+      <a href='#'>Home</a>
+      <a href='#'>New Feed</a>
+      <a href='#'>Messages</a>
+    </div>
+  );
+}
+const Technologies = () => {
+  return (
+    <div className="technologies">
       <ul>
         <li>css</li>
         <li>js</li>
