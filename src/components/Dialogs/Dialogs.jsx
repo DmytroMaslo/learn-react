ec832a2e116c42e97fc3566e1ca6cc7850997951
DmytroMaslo <maslo.dmitry@gmail.com>
2023-02-21 13:16:26 +0200

    dialogs html+css

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,9 +1,29 @@
 import React from "react"
-const Dialogs = ()=>{
+import classes from './Dialogs.module.css'
+const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={classes.dialogsWrapper}>
+            <div className={classes.dialogs}>
+                <div className={classes.dialog}>
+                    Alex
+                </div>
+                <div className={classes.dialog + ' ' + classes.activeDialog}>
+                    Nick
+                </div>
+                <div className={classes.dialog}>
+                    Sam
+                </div>
+                <div className={classes.dialog}>
+                    Daniel
+                </div>
+            </div>
+            <div className={classes.messages}>
+                <div className={classes.message}>Hi!</div>
+                <div className={classes.message}>How are you?</div>
+                <div className={classes.message}>It's ok</div>
+            </div>
         </div>
+
     )
 }
 
