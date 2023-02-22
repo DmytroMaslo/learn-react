4e5744e3014e41e1b0e5e302f6fcfa1ee6d05605
DmytroMaslo <maslo.dmitry@gmail.com>
2023-02-22 10:15:00 +0200

    mock data

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -15,6 +15,22 @@ const Message = (props)=>{
     )
 }
 const Dialogs = () => {
+    let DialogData = [
+        {id:1,name:'Alex'},
+        {id:2,name:'Nick'},
+        {id:3,name:'Sam'},
+        {id:4,name:'Daniel'},
+        {id:5,name:'Viktor'},
+        {id:6,name:'Dean'},
+    ];
+    let MessageData = [
+        {id:1,message:'Hi!'},
+        {id:2,message:'How are you?'},
+        {id:3,message:'Okey'},
+        {id:4,message:'Yo!'},
+        {id:5,message:'Yo@'},
+        {id:6,message:'Yo?'},
+    ]
     return (
         <div className={styles.dialogsWrapper}>
             <div className={styles.dialogs}>
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,6 +3,14 @@ import style from './MyPosts.module.css'
 import Post from "./Post/Post";
 
 const MyPosts = () => {
+    let PostData = [
+        {id:1,post:'Hi! first message',likes:3},
+        {id:2,post:'Using props!',likes:5},
+        {id:3,post:'How are you?',likes:7},
+        {id:4,post:'Spam message',likes:4},
+        {id:5,post:'Just another post',likes:9},
+        {id:6,post:'More post is better',likes:13},
+    ]
     return (
         <div>
             <div className={style.addPost}>
