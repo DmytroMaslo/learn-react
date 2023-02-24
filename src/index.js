d1b6049ffae8ee53bcf396a009a250a535cbad82
DmytroMaslo <maslo.dmitry@gmail.com>
2023-02-24 16:52:44 +0200

    store state bind

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
 import reportWebVitals from './reportWebVitals';
+import store from './redux/state.js'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
 import './fonts/headerFont.ttf';
-import state from './redux/state.js'
-import { rerenderEntireTree } from './render';
 
 
-rerenderEntireTree(state);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+let rerenderEntireTree = (state) => {
+  root.render(
+    <React.StrictMode>
+      <App
+        appState={store.getState()}
+        addPost={store.addPost.bind(store)}
+        updateNewPostText = {store.updateNewPostText.bind(store)} />
+    </React.StrictMode>
+  );
+}
+
+rerenderEntireTree(store._state);
+
+store.subscribe(rerenderEntireTree);
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,52 +1,60 @@
-import { rerenderEntireTree } from "../render";
+let store = {
+  _state: {
+    dialogsPage: {
+      dialogs: [
+        { id: 1, name: 'Alex' },
+        { id: 2, name: 'Nick' },
+        { id: 3, name: 'Sam' },
+        { id: 4, name: 'Daniel' },
+        { id: 5, name: 'Viktor' },
+        { id: 6, name: 'Dean' },
+      ],
+      messages: [
+        { id: 1, message: 'Hi!' },
+        { id: 2, message: 'How are you?' },
+        { id: 3, message: 'Okey' },
+        { id: 4, message: 'Yo!' },
+        { id: 5, message: 'Yo@' },
+        { id: 6, message: 'Yo?' },
+        { id: 7, message: '%#&$#$?' },
+      ]
+    },
+    profilePage: {
+      posts: [
+        { id: 1, post: 'Hi! first message', likes: 3 },
+        { id: 2, post: 'Using props!', likes: 5 },
+        { id: 3, post: 'How are you?', likes: 7 },
+        { id: 4, post: 'Spam message', likes: 4 },
+        { id: 5, post: 'Just another post', likes: 9 },
+        { id: 6, post: 'More post is better', likes: 13 },
+      ],
+      newPostText: 'data from state'
+    }
 
-
-let state = {
-  dialogsPage: {
-    dialogs: [
-      { id: 1, name: 'Alex' },
-      { id: 2, name: 'Nick' },
-      { id: 3, name: 'Sam' },
-      { id: 4, name: 'Daniel' },
-      { id: 5, name: 'Viktor' },
-      { id: 6, name: 'Dean' },
-    ],
-    messages: [
-      { id: 1, message: 'Hi!' },
-      { id: 2, message: 'How are you?' },
-      { id: 3, message: 'Okey' },
-      { id: 4, message: 'Yo!' },
-      { id: 5, message: 'Yo@' },
-      { id: 6, message: 'Yo?' },
-      { id: 7, message: '%#&$#$?' },
-    ]
   },
-  profilePage: {
-    posts: [
-      { id: 1, post: 'Hi! first message', likes: 3 },
-      { id: 2, post: 'Using props!', likes: 5 },
-      { id: 3, post: 'How are you?', likes: 7 },
-      { id: 4, post: 'Spam message', likes: 4 },
-      { id: 5, post: 'Just another post', likes: 9 },
-      { id: 6, post: 'More post is better', likes: 13 },
-    ],
-    newPostText:'data from state'
+  getState(){
+    return this._state;
+  },
+  _callSubscriber() {
+    console.log('State changed');
+  },
+  addPost() {
+    let newPost = {
+      id: 7,
+      post: this._state.profilePage.newPostText,
+      likes: 0
+    };
+    this._state.profilePage.posts.push(newPost)
+    this._state.profilePage.newPostText = '';
+    this._callSubscriber(this._state);
+  },
+  updateNewPostText(newText) {
+    this._state.profilePage.newPostText = newText;
+    this._callSubscriber(this._state);
+  },
+  subscribe(observer){
+    this._callSubscriber = observer;
   }
-
 }
-window.state = state;
-export let addPost = () => {
-  let newPost = {
-    id: 7,
-    post: state.profilePage.newPostText,
-    likes: 0
-  };
-  state.profilePage.posts.push(newPost)
-  state.profilePage.newPostText = '';
-  rerenderEntireTree(state);
-}
-export let updateNewPostText = (newText) => {
-  state.profilePage.newPostText = newText;
-  rerenderEntireTree(state);
-}
-export default state;
+window.store = store;
+export default store;
